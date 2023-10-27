a472c593236752cb86e1927ad02d12ca00929d40
Anurag Bhandari <anuragbhandari77736@gmail.com>
2023-10-27 13:19:44 +0530

    "Just remembering the structure of redux"

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Navbar from './Components/Navbar'
+import Counter from './Components/Counter'
+
+
+export default function App() {
+  return (
+    <>
+    <div className="min-h-full min-w-full">
+    <Navbar/>
+    <Counter/>
+    </div>
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { store } from './app/store.js'
+import { Provider } from 'react-redux'
+import MainContextProvider from './Contexts/MainContext.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store = {store}>
+    <MainContextProvider>
+    <App />
+    </MainContextProvider>
+    </Provider>
+  </React.StrictMode>,
+)
